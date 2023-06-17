45a3812b0e6999e47157c1a71d634254fe1f70ea
nuthan <inbox.nuthan@gmail.com>
2023-06-17 23:39:55 +0530

    Created a model/Entity classes for a tables to generate. Entity Names are League and Team

---
+++ EntityFrameworkCore.Domain/League.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkCore.Domain
+{
+    public class League
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
---
+++ EntityFrameworkCore.Domain/Team.cs
@@ -0,0 +1,18 @@
+﻿namespace EntityFrameworkCore.Domain
+{
+    public class Team
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        // <TableName><ColumnName> => ForiegnKey
+        public int LeagueId { get; set; }
+
+        // Navigation Property
+        public virtual League League { get; set; }
+
+        // LeagueId and League consider EFCore is nothing for foriegnKey relationship
+
+    }
+}
